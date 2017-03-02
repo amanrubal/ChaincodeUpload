5c43b11086cb084baaec431622db001665956c6f
amanrubal <amandeep_singh1210@yahoo.com>
2017-03-02 14:23:36 -0600

    Update BCRoam.go

--- BCRoam.go
+++ BCRoam.go
@@ -121,10 +121,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 }*/
 
 //QUERY FUNCTION
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function args) ([]byte, error) {
 	fmt.Println("======== Query called, determining function")
 	var user string
-	user= args[0]
+	user= args
 	fmt.Printf("User name: %v",user)
 	bytes, err := stub.GetState(user)
 	var peer string
