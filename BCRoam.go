0a919492782ebe46a28884ff85d0a4548dd60285
amanrubal <amandeep_singh1210@yahoo.com>
2017-05-25 21:13:22 -0500

    Update BCRoam.go

--- BCRoam.go
+++ BCRoam.go
@@ -150,8 +150,7 @@ func (t *SimpleChaincode) resetInventory(stub shim.ChaincodeStubInterface) ([]by
 	rs6 := rsDetailBlock{"rs6", "349091234568", "F", "BARCELONA", "XYZ", "", "FALSE", "BARCELONA","41.385064","2.173403","", "", "", "", 0.0, 0.0, "", currtime}
 	rs7 := rsDetailBlock{"rs7", "349091234569", "G", "BARCELONA", "XYZ", "", "FALSE", "BARCELONA","41.385064","2.173403","", "", "", "", 0.0, 0.0, "", currtime}
 
-    if len(rsmap) != 0
-	{
+    if len(rsmap)!= 0{
 	rsmap["rs1"] = "14691234567"
 	rsmap["rs2"] = "14691234568"
 	rsmap["rs3"] = "14691234569"
